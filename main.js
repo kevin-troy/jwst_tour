539ade259048726b511d7b1a1ecd882bcc0ab58a
kevin-troy <90881154+kevin-troy@users.noreply.github.com>
2022-09-30 02:59:36 -0700

    Updated three import

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import './style.css'
-import * as THREE from 'three';
+//import * as THREE from 'three';
+import * as THREE from "/node_modules/three/build/three.module.js";
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { RGBELoader } from 'three/addons/loaders/RGBELoader.js';
 import { OrbitControls } from '/node_modules/three/examples/jsm/controls/OrbitControls'
